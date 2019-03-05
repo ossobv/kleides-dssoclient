749825855f660e38aace5e48207f1a337b8d442b
Harm Geerts <hgeerts@osso.nl>
2019-03-05 17:49:31 +0100

    Add logging to help investigate problems

--- kleides_dssoclient/middleware.py
+++ kleides_dssoclient/middleware.py
@@ -1,3 +1,4 @@
+import logging
 import random
 import time
 
@@ -12,8 +13,10 @@ try:
 except ImportError:
     MiddlewareMixin = object
 
-
 from .dssoclient import DssoClientDecoder, DssoClientEncoder
+
+log = logging.getLogger(__name__)
+log.addHandler(logging.NullHandler())
 
 
 class DssoLoginMiddleware(MiddlewareMixin):
@@ -39,6 +42,7 @@ class DssoLoginMiddleware(MiddlewareMixin):
         # The user is already authenticated? Nothing to do.
         if (request.user.is_authenticated if django.VERSION >= (1, 10)
                 else request.user.is_authenticated()):
+            log.debug('User %r is authenticated, skipping DSSO', request.user)
             return
 
         if not (settings.KLEIDES_DSSO_ENDPOINT and
@@ -55,13 +59,16 @@ class DssoLoginMiddleware(MiddlewareMixin):
         elif 'kleides_dsso_nonce' in request.session:
             return self.return_from_dsso_endpoint(request)
 
-        # This is bad..? Session not saved?
+        # This is bad..? Session not saved? Or an error during processing?
+        # Shouldn't redirect because that may start a loop if the failure
+        # is persistant.
         raise SuspiciousOperation(
             'Session data corrupt? Or user playing around?')
 
     def redirect_to_dsso_endpoint(self, request):
         # Construct nonce by using timestamp and a bit of random junk.
         nonce = '{}-{}'.format(int(time.time()), random.random())
+        log.debug('Starting DSSO authentication with nonce: %r', nonce)
         return_url = request.build_absolute_uri()
         try:
             enc = DssoClientEncoder(
@@ -69,6 +76,7 @@ class DssoLoginMiddleware(MiddlewareMixin):
                 nonce, return_url)
             uri = enc.get_redirect_url(settings.KLEIDES_DSSO_ENDPOINT)
         except ValueError as e:
+            log.debug('Error encoding DSSO payload: %r', e, exc_info=True)
             raise PermissionDenied(str(e))
 
         request.session['kleides_dsso_nonce'] = nonce
@@ -79,8 +87,10 @@ class DssoLoginMiddleware(MiddlewareMixin):
 
     def return_from_dsso_endpoint(self, request):
         sso_nonce = request.session.pop('kleides_dsso_nonce')
+        log.debug('Returned from DSSO endpoint with nonce: %r', sso_nonce)
         sso_time, sso_rand = sso_nonce.split('-', 1)
         if (time.time() - int(sso_time)) > 60:
+            log.debug('stale kleides_dsso_nonce in session')
             raise PermissionDenied('stale kleides_dsso_nonce in session')
 
         try:
@@ -89,13 +99,16 @@ class DssoLoginMiddleware(MiddlewareMixin):
                 sso_nonce, request.get_full_path())
             mapping = dec.get_mapping()
         except ValueError as e:
+            log.debug('Error decoding DSSO payload: %r', e, exc_info=True)
             raise PermissionDenied(str(e))
 
         user = auth.authenticate(dsso_mapping=mapping)
         if not user:
+            log.debug('DSSO mapping did not yield a user')
             raise PermissionDenied('not for you')
 
         # User is valid.  Set request.user and persist user in the session
         # by logging the user in.
         request.user = user
         auth.login(request, user)
+        log.debug('Logged in %r using kleides_dssoclient', user)
